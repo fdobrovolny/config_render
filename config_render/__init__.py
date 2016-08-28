201e58c4ad1f6fbfc43eb9b70456ffe94b857b74
Filip Dobrovolny <brnopcman@gmail.com>
2016-08-28 12:51:03 +0200

    Added config_render.config_render FileTemplateLoader, ConfigRender
    FileTemplateLoader - load templates from anywhere in the system
    ConfigRender - Work with jinja2 rendering

--- config_render/__init__.py
+++ config_render/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from .config_render import ConfigRender
 __version__ = '0.0.1a1'
+
+__all__ = ["ConfigRender"]
---
+++ config_render/config_render.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+
+from jinja2 import Environment
+from jinja2 import BaseLoader, TemplateNotFound
+from os.path import join, exists, getmtime
+
+
+class FileTemplateLoader(BaseLoader):
+    """Load template from specified path."""
+
+    def get_source(self, environment, template):
+                if not exists(template):
+                    raise TemplateNotFound(template)
+                mtime = getmtime(template)
+                with file(template) as f:
+                    source = f.read().decode('utf-8')
+                return source, template, lambda: mtime == getmtime(template)
+
+
+class ConfigRender(object):
+    """Class to interact with Jinja2."""
+    def __init__(self, context={}, loader=FileTemplateLoader(), environment_args=[],
+                 environment_kwargs={}):
+        """
+        Initialize ConfigRender.
+
+        :param context: Variables to be passed into template
+        :type context: Dict
+        :param loader: Jinja2 Loader
+        :type loader: jinja2.BaseLoader
+        :param environment_args: Args to be passed to Enviroment
+        :type environment_args: list
+        :param environment_kwargs: Kwargs to be passed to Enviroment
+        :param environment_kwargs: Dict
+        """
+        self.context = context
+        self.environment = Environment(*environment_args, loader=loader, **environment_kwargs)
+
+    def write(self, template_name, file, context={}):
+        """
+        Render and write template to file
+
+        :param template_name: Name of the Template
+        :type template_name: String
+        :param file: File object to write it into it
+        :type file: file
+        :param context: Dictionary by which content will be updated local copy of
+        ConfigRender context
+        :type context: Dict
+        """
+        final_context = dict(self.context)
+        final_context.update(context)
+        template = self.environment.get_template(template_name)
+        file.write(template.render(**final_context))
